c5f9bef87d44b2e7ad8f8d553e496c20d5da8870
georgius1024 <georgius@ural.ru>
2021-01-03 20:07:41 +0500

    basic store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuex from 'vuex'
 import vmodal from 'vue-js-modal'
+import App from './App.vue'
+import store from './store'
+Vue.use(Vuex)
 Vue.use(vmodal)
+new Vuex.Store(store)
 Vue.config.productionTip = false
 
 new Vue({
